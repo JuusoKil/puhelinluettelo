fbad5d874b117d5ee9fb4804c1642ab7a668899f
Juuso Kilponen <juuso.kilponen@hotmail.com>
2023-02-15 20:34:13 +0200

    Port change

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
